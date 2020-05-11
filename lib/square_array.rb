453fb3b2e5d3cf577c2a45b4a8f5de729db0097f
Danira Cortez <cortezd334@gmail.com>
2020-05-11 18:03:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
- # count = 0
+  count = 0
   new_array = []
   
-  while array.length |index|
+  while count < array.length do
     new_array << index**2
-    #count += 1
+    count += 1
   end
   new_array
 end
